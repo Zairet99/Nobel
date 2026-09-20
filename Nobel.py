bd38d64cc9ae44c564d47a41fe2ce512b814e8ea
Zairet99 <zperezv001@alumno.uaemex.mx>
2026-09-19 21:29:32 -0600

    Update CSV encoding for nobel_unido.csv
    Change encoding from 'utf-8' to 'latin-1' for CSV file.

--- Nobel.py
+++ Nobel.py
@@ -20,7 +20,7 @@ def user_input_features():
 
 df = user_input_features()
 
-nobel =  pd.read_csv('nobel_unido.csv', encoding='utf-8')
+nobel =  pd.read_csv('nobel_unido.csv', encoding='latin-1')
 X = nobel.Motivation
 y = nobel.Category
 
